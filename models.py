3c6efab83b174b5b9f11116bb9379ec7fc2e35e0
Ricardo Magalhães <29683600+AmodeusR@users.noreply.github.com>
2025-07-03 11:52:42 -0300

    refactor: remove unused resource

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, Table
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
